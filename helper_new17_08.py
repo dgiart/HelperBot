2ab663a827da99fe81dc036ad251c70b68d97398
artem babich <artem_babich@epam.com>
2022-09-03 17:08:41 +0300

    03/09/22, 17.08, host added

--- helper_new17_08.py
+++ helper_new17_08.py
@@ -742,4 +742,5 @@ if __name__ == '__main__':
     print(sys.version)
     print(time.asctime())
     # ff = jj
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
