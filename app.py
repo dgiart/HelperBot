b8b97f620cc9c8c61a1f6def1f6c6195a5525c72
artem babich <artem_babich@epam.com>
2022-09-13 14:31:00 +0300

    14.30, 13/09 to new new server simple app added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'new 13.09 at {asctime()}'
+    return f'<h1> New 13.09 at {asctime()} </h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
