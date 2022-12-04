21a053c1e4fda3b4efa2a99279e49659c653becc
artem babich <artem_babich@epam.com>
2022-12-04 20:12:10 +0200

    4.12 check+xost changed+

--- helper_new17_08.py
+++ helper_new17_08.py
@@ -730,4 +730,5 @@ if __name__ == '__main__':
     # print(time.asctime())
     # ff = jj
     # app.run(debug=True, port=5000)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
